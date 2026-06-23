6a9f27d4009a768ec711056eb377bd3770df9ba0
Violet-King-Dev10 <obij77708@gmail.com>
2026-06-23 01:12:20 +0100

    Delete VioletFlixTV directory

--- VioletFlixTV/hooks/useWatchlist.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { WatchlistContext } from '@/contexts/WatchlistContext';
-
-export function useWatchlist() {
-  const ctx = useContext(WatchlistContext);
-  if (!ctx) throw new Error('useWatchlist must be used within WatchlistProvider');
-  return ctx;
-}
